5b3b6025817452f7b23dc57d6ee70e896276b596
Wiesz1 <pwiesz@gmail.com>
2021-11-04 15:49:03 +0000

    Random Domain Generator is working

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "my", "their"];
+  let adj = ["great", "big", "small", "tall"];
+  let noun = ["jogger", "racoon", "dog", "cat"];
+  let ending = [".com", ".net", ".io", ".org"];
+
+  for (let i = 0; i < 10; i++) {
+    let pronounindex = Math.floor(Math.random() * pronoun.length);
+    let adjindex = Math.floor(Math.random() * adj.length);
+    let nounindex = Math.floor(Math.random() * noun.length);
+    let endingindex = Math.floor(Math.random() * ending.length);
+
+    let sentence =
+      pronoun[pronounindex] +
+      adj[adjindex] +
+      noun[nounindex] +
+      ending[endingindex];
+
+    document.getElementById(i).innerHTML = sentence;
+  }
 };
